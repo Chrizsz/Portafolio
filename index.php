2a80bbaa279a212ca4c374ca6dd677c69434e80a
Chrizsz <45052899+Chrizsz@users.noreply.github.com>
2018-11-15 13:14:39 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Christian Gmz</title>
+    <title>Portafolio</title>
     <!-- Hoja de estilos bootstrap -->
     <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
